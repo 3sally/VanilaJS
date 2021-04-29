41da30ec612995910cc734a94c0adeb0956f12a6
minjunglee <3sally@naver.com>
2021-04-29 19:21:52 +0900

    Save the Image

--- NomadCoders.Canvas/app.js
+++ NomadCoders.Canvas/app.js
@@ -3,12 +3,16 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "black";
 const CANVAS_SIZE = 700;
 
+
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE); //canvas의 background color white로 설정
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -59,9 +63,20 @@ function handleModeClick(){
 
 function handleCanvasClick(){
     if(filling){
-         ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
     }
-   
+}
+
+function handleRightClick(event){
+    event.preventDefault(); // 이 이벤트가 실행되지 않도록 설정
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "canvas.jpeg";
+    link.click();
 }
 
 if(canvas){
@@ -70,6 +85,7 @@ if(canvas){
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
     canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu",handleRightClick);
 }
 
 Array.from(colors).forEach(color=>color.addEventListener("click",handleColorClick));
@@ -80,4 +96,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click",handleModeClick);
+}
+
+if(save){
+    save.addEventListener("click",handleSaveClick);
 }
